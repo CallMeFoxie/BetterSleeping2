77134feadbeb6753b632da5e5bf4d691799ba06b
Ashley <ashley@victorianfox.com>
2016-01-24 13:35:47 +0100

    Remove old and deprecated EventHandlers code

--- src/main/java/foxie/bettersleeping/proxy/ProxyCommon.java
+++ src/main/java/foxie/bettersleeping/proxy/ProxyCommon.java
@@ -1,20 +1,17 @@
 package foxie.bettersleeping.proxy;
 
-import foxie.bettersleeping.EventHandlers;
 import foxie.bettersleeping.network.Network;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ProxyCommon {
    public void preinit(FMLPreInitializationEvent event) {
       new Network();
-      new EventHandlers();
    }
 
    public void init(FMLInitializationEvent event) {
-      MinecraftForge.EVENT_BUS.register(EventHandlers.INSTANCE);
+
    }
 
    public EntityPlayer getPlayer() {
